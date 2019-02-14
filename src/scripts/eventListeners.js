f383292b573a1b7781bcb362f3b0b1740eb4b687
Panya Farnette <naienko@gmail.com>
2019-02-14 14:14:16 -0600

    changed starter code to use eventsModule methods

--- src/scripts/eventListeners.js
+++ src/scripts/eventListeners.js
@@ -5,17 +5,19 @@ Task: listen to the body of the page for clicks, and call other methods based on
 
 import APIManager from "./APIManager";
 import printToDOM from "./printToDOM";
-import chatsModule from "./chats";
+import eventsModule from "./eventsModule";
 
 const clickBubbler = {
     listener: () => {
         document.querySelector("#dashboardContainer").addEventListener("click", event => {
             if (event.target === "button") {
                 const targetList = event.target.id.split("--");
+                const where = targetList[0] + "Display";
+                let newObject = {};
                 if (targetList[1] === "create") {
-                    // call the correct object factory based on targetList[0], which should contain the module name (i.e. 'events') 
-                    if (targetList[0] === "chats") {
-                        const newObject = chatsModule.buildChatsObject();
+                    // call the correct object factory based on targetList[0], which should contain the module name (i.e. 'events')
+                    if (targetList[0] === "events") {
+                        newObject = eventsModule.createEventObject();
                     }
                     // then call the api create method and pass it the new object and the module name
                     APIManager.Post(targetList[0], newObject)
@@ -24,42 +26,40 @@ const clickBubbler = {
                         objectArray => {
                             let newHTMLstring = "";
                             objectArray.forEach(element => {
-                                if (targetList[0] === "chats") {
-                                    newHTMLstring += chatsModule.buildChatsHTML(element);
+                                if (targetList[0] === "events") {
+                                    newHTMLstring += eventsModule.createEventHTML(element);
                                 }
                             });
                             // call printToDom() and pass it the new HTML string
-                            const where = targetList[0] + "Container";
                             printToDOM(newHTMLstring, where);
                         }
                     )
                 } else if (targetList[1] === "edit") {
-                    // call the correct object factory based on targetList[0], which should contain the module name (i.e. 'events') 
-                    if (targetList[0] === "chats") {
-                        const newObject = chatsModule.buildChatsObject();
+                    // call the correct object factory based on targetList[0], which should contain the module name (i.e. 'events')
+                    if (targetList[0] === "events") {
+                        newObject = eventsModule.createEventObject();
                     }
                     // then call the api edit method and pass it the new object, the module name, and the original object id
                     //desiredDatabase, objectId, editedObject
-                    let chatId = document.querySelector("#chatId");
-                    APIManager.Put(targetList[0], chatId, newObject)
+                    let eventId = document.querySelector("#eventId");
+                    APIManager.Put(targetList[0], eventId, newObject)
                     // .then() and call the create HTML method from the correct module, using the returned Promise from api method to fill it
                     .then(
                         objectArray => {
                             let newHTMLstring = "";
                             objectArray.forEach(element => {
-                                if (targetList[0] === "chats") {
-                                    newHTMLstring += chatsModule.buildChatsHTML(element);
+                                if (targetList[0] === "events") {
+                                    newHTMLstring += eventsModule.createEventHTML(element);
                                 }
                             });
                             // call printToDom() and pass it the new HTML string
-                            const where = targetList[0] + "Container";
                             printToDOM(newHTMLstring, where);
                         }
                     )
                 } else if (targetList[1] === "delete") {
                     // call the api delete method and pass it the module name and the original object id
-                    let chatId = document.querySelector("#chatId");
-                    APIManager.delete(targetList[0], chatId)
+                    let eventId = document.querySelector("#eventId");
+                    APIManager.delete(targetList[0], eventId)
                     // .then() and call the api list method, passing it the correct module and userid
                     .then(
                         () => {
@@ -69,12 +69,11 @@ const clickBubbler = {
                                 objectArray => {
                                     let newHTMLstring = "";
                                     objectArray.forEach(element => {
-                                        if (targetList[0] === "chats") {
-                                            newHTMLstring += chatsModule.buildChatsHTML(element);
+                                        if (targetList[0] === "events") {
+                                            newHTMLstring += eventsModule.createEventHTML(element);
                                         }
                                     });
                                     // call printToDom() and pass it the new HTML string
-                                    const where = targetList[0] + "Container";
                                     printToDOM(newHTMLstring, where);
                                 }
                             )
