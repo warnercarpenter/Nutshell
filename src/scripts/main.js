ae8ffacb58b3866e7ee2c1f23b1569fa8aa57f86
Panya Farnette <naienko@gmail.com>
2019-02-14 11:37:44 -0600

    cleared main.js for merge

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,3 @@
 // import event listeners module from "./eventlisteners"
 
 // hello world
-
-import eventsModule from "./eventsModule";
-import printToDOM from "./printToDOM";
-
-printToDOM(eventsModule.buildEntryForm(),"#eventDisplay");
