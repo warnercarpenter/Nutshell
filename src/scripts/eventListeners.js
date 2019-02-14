db7ca694493641ed05f1ff1ef918f6d73a6d9f1e
Panya Farnette <naienko@gmail.com>
2019-02-14 12:03:09 -0600

    begun the listener

---
+++ src/scripts/eventListeners.js
@@ -0,0 +1,14 @@
+/*
+Author: Panya
+Task: listen to the body of the page for clicks, and call other methods based on the target of the click
+*/
+
+const clickBubbler = {
+    listener: () => {
+        document.body.addEventListener("click", event => {
+            
+        })
+    }
+}
+
+export default clickBubbler;
