eb99baa5292568a211287e8c4fa4b5606b2b7c0e
Warner Carpenter <warnercarpenter@yahoo.com>
2019-02-14 11:03:31 -0600

    chats file version 1

---
+++ src/scripts/chats.js
@@ -0,0 +1,34 @@
+const chatsModule = {
+    buildChatsForm: () => {
+        return `
+            <div id="chatsForm">
+                Enter your message:</br>
+                <textarea rows="4" cols="50" name="chatMessage" id="chat--textInput"></textarea></br>
+                <button id="chat--submit">Submit</button>
+            </div>
+        `
+    },
+    buildChatsObject: () => {
+        const chatsObject = {}
+        chatsObject.text = document.getElementById("chat--textInput").value
+        chatsObject.timestamp = Date.now()
+        return chatsObject
+    },
+    buildChatsHTML: (chatObject) => {
+        timestamp = String(new Date(chatObject.timestamp))
+        cutTimestamp = timestamp.split(" ")
+        cutTimestamp.length = 4
+        cutTimestamp.splice(0, 1, `${cutTimestamp[0]}.`)
+        cutTimestamp.splice(1, 1, `${cutTimestamp[1]},`)
+        formattedTimestamp = cutTimestamp.join(" ")
+
+        return `
+            <div id="chat--${chatObject.id}"
+                <p class="chatTextContent">${chatObject.text}</p>
+                <p class="chatSubText">Posted by ${chatObject.user.username} on ${formattedTimestamp}</p>
+            </div>
+        `
+    }
+}
+
+export default chatsModule
