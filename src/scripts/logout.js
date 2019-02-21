87e8b3d6ae7f2b61f18d587343ba2c3b4267fb0a
Mo Silvera <mo.leslie.silvera@gmail.com>
2019-02-20 19:02:08 -0600

    fixed the stacking event listeners

--- src/scripts/logout.js
+++ src/scripts/logout.js
@@ -5,6 +5,7 @@ const landing = () => {
     <div id="listenToMe">
     <button id="register--link">Register</button> | <button id="login--link">Login</button>
     </div>`;
+    document.querySelector("#listenerBlock").outerHTML = document.querySelector("#listenerBlock").outerHTML
     document.querySelector("#formSection").innerHTML = HTMLcode;
     dashboardContainer.classList.add("hidden");
     clickBubbler.firstLoad();
